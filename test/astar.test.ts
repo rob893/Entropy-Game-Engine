e9b77c5b6aadadb2e200447ad62121aace00a375
Robert Herber <rherber@nextcenturi.com>
2019-07-29 08:56:31 -0700

    Fixed issue in astar test where grid was not being built correctly.

--- test/astar.test.ts
+++ test/astar.test.ts
@@ -2,15 +2,17 @@ import { AStarSearch } from '../src/GameEngine/Core/Helpers/AStarSearch';
 import { NavGrid } from '../src/GameEngine/Core/Helpers/NavGrid';
 import {toBeDeepCloseTo, toMatchCloseTo} from 'jest-matcher-deep-close-to';
 import { Vector2 } from '../src/GameEngine/Core/Helpers/Vector2';
+import { IWeightedGraph } from '../src/GameEngine/Core/Interfaces/IWeightedGraph';
+import { IWeightedGraphCell } from '../src/GameEngine/Core/Interfaces/IWeightedGraphCell';
 expect.extend({toBeDeepCloseTo, toMatchCloseTo});
 
 test('Test the a star path finding', () => {
     const graph = new NavGrid(1);
 
     let graphVisual = '';
-    for (let x = 0; x < 10; x++) {
-        for (let y = 0; y < 10; y++) {
-            if (y === 4 && x < 7 && x > 2) {
+    for (let y = 0; y < 10; y++) {
+        for (let x = 0; x < 10; x++) {
+            if (x === 4 && y < 7 && y > 2) {
                 graph.addCell({ passable: false, weight: 0, position: new Vector2(x, y) });
                 graphVisual += 'X '
             }
@@ -18,10 +20,8 @@ test('Test the a star path finding', () => {
                 graph.addCell({ passable: true, weight: 0, position: new Vector2(x, y) });
                 graphVisual += '* ';
             }
-            if (y === 9) {
-                graphVisual += '\n';
-            }
         }
+        graphVisual += '\n';
     }
     console.log(graphVisual);
 
@@ -36,7 +36,6 @@ test('Test the a star path finding', () => {
     path = AStarSearch.findPath(graph, new Vector2(0, 4), new Vector2(4, 4));
 
     expect(path).toBe(null);
-    debugger;
     path = AStarSearch.findPath(graph, new Vector2(0, 4), new Vector2(6, 4));
     console.log(path);
 });
