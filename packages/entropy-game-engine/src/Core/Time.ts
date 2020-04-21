72fad21964f8270652097ab6073a5ff17f13f8a7
Robert Herber <rwherber@gmail.com>
2020-04-21 07:34:06 -0700

    test: add test for time

--- packages/entropy-game-engine/src/Core/Time.ts
+++ packages/entropy-game-engine/src/Core/Time.ts
@@ -1,30 +1,23 @@
 export class Time {
     private _deltaTime: number = 0;
     private _totalTime: number = 0;
-    private startTime: number = 0;
-    private prevTime: number = 0;
+    private prevTime: number | null = null;
 
     public get deltaTime(): number {
         return this._deltaTime;
     }
 
     public get totalTime(): number {
-        return this._totalTime / 1000;
+        return this._totalTime;
     }
 
-    // public start(): void {
-    //     this.prevTime = Date.now();
-    //     this.startTime = this.prevTime;
-    // }
-
-    public updateTime(timeStamp: number): void {
-        // this._deltaTime = (Date.now() - this.prevTime) / 1000;
-        // this.prevTime = Date.now();
-        if (this.prevTime === 0) {
-            this.prevTime = timeStamp;
+    public updateTime(timeSincePageLoad: number): void {
+        if (this.prevTime === null) {
+            this.prevTime = timeSincePageLoad;
         }
-        this._deltaTime = (timeStamp - this.prevTime) / 1000;
-        this.prevTime = timeStamp;
+
+        this._deltaTime = (timeSincePageLoad - this.prevTime) / 1000;
+        this.prevTime = timeSincePageLoad;
         this._totalTime += this._deltaTime;
     }
 }
